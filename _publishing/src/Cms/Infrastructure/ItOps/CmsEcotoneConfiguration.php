69aa20a66a385b761dfa7afb0668b0ef1f2d2ae5
zerai <teclaizerai@gmail.com>
2023-06-04 09:58:01 +0200

    Disabled ecotone.

--- _publishing/src/Cms/Infrastructure/ItOps/CmsEcotoneConfiguration.php
+++ _publishing/src/Cms/Infrastructure/ItOps/CmsEcotoneConfiguration.php
@@ -25,7 +25,9 @@ class CmsEcotoneConfiguration
     #[ServiceContext]
     public function documentStoreRepository()
     {
-        return DbalConfiguration::createWithDefaults()
-            ->withDocumentStore(enableDocumentStoreAggregateRepository: true);
+        //        return DbalConfiguration::createWithDefaults()
+        //            ->withDocumentStore(enableDocumentStoreAggregateRepository: true);
+
+        return [];
     }
 }
