e5e38023f44d2d3c19b9cb1bbd77fa5d0494acd4
zerai <teclaizerai@gmail.com>
2023-06-04 09:57:20 +0200

    Composer deps. doctrine.

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
